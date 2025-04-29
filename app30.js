eeff1e3f88acf657ec79cc6593a0440381f266e2
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-29 19:07:43 +0530

    Add files via upload

---
+++ app30.js
@@ -0,0 +1,8 @@
+let str = "ApNaCoLlEgE";
+let idx = 0;
+if (str [idx] == str [idx].toLowerCase()) {
+    console.log("charcter is lowercase");
+} 
+else {
+    console.log("character is not lowercase");
+}
